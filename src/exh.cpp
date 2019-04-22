12fa906a11cbdffb75d09eec6c16c2729f21d2c8
LIU Hao <hliubs@rwcpu1.cse.ust.hk>
2019-04-22 20:16:55 +0800

    modify exh.cpp to release the memory needed

--- src/exh.cpp
+++ src/exh.cpp
@@ -6,22 +6,37 @@
 #include <vector>
 using namespace std;
 
+#define MAX_ARRAY_SIZE 50000
+
 vector<double> radiis;
 
+int dump_array(ofstream &ofs) {
+	int size = radiis.size();
+	for (auto &i: radiis) {
+		ofs << i << endl;
+	}
+	radiis.clear();
+	return size;
+}
+
 long global_counter = 0;
-void next_glb_idx(long total) {
+void next_glb_idx(long total, ofstream &ofs) {
 	long base = total / 100;
-	if (global_counter % base == 0) {
+	if (global_counter % base == 0 && global_counter > 0) {
+		if (total > MAX_ARRAY_SIZE) {
+			int piece_size = dump_array(ofs);
+			cout << piece_size << " items written\n";
+		}
 		cout << (global_counter / base) << "%...\n";
 	}
 	global_counter++;
 }
 
-void combination_tetra(long res_size, Pt3D list[], Pt3D* tetra[], int start, int end, int index) {
+void combination_tetra(long res_size, Pt3D list[], Pt3D* tetra[], int start, int end, int index, ofstream &ofs) {
 	if (index == 4) {
 		double r = circumradi_3d(*tetra[0], *tetra[1], *tetra[2], *tetra[3]);
+		next_glb_idx(res_size, ofs);
 		radiis.push_back(r);
-		next_glb_idx(res_size);
 
 		// print_pt3d(tetra[0]);
 		// cout << endl;
@@ -36,11 +51,16 @@ void combination_tetra(long res_size, Pt3D list[], Pt3D* tetra[], int start, int
 
 	for (int i = start; i <= end && end - i >= 3 - index; i++) {
 		tetra[index] = &list[i];
-		combination_tetra(res_size, list, tetra, i + 1, end, index + 1);
+		combination_tetra(res_size, list, tetra, i + 1, end, index + 1, ofs);
 	}
 }
 
 int main(int argc, char **argv) {
+	if (argc < 2) {
+		cerr << "missing output file" << endl;
+		return 1;
+	}
+
 	ifstream input_ply;
 	input_ply.open("bun_zipper_res4.ply");
 	string line;
@@ -49,9 +69,12 @@ int main(int argc, char **argv) {
 		getline(input_ply, line);
 	}
 	int size = stoi(line.substr(line.find_last_of(' ')));
-	cout << size << endl;
+	string output_file;
 
-	if (argc > 1) {
+	if (argc == 2) {
+		output_file = argv[1];
+	} else if (argc > 2) {
+		output_file = argv[2];
 		size = atoi(argv[1]);
 		cout << "size adjusted to " << size << endl;
 	}
@@ -71,21 +94,24 @@ int main(int argc, char **argv) {
 	}
 	input_ply.close();
 
+	ofstream output_radi(output_file);
+	if (!output_radi) {
+		cerr << "cannot open output file " << output_file << endl;
+		return 1;
+	}
+
 	long perm_size = size;
 	perm_size *= (size - 1) * (size - 2) * (size - 3) / 24;
-	cout << perm_size << endl;
+	cout << "permutation size is " << perm_size << endl;
 
 	Pt3D* tetra[4];
-	combination_tetra(perm_size, point_list, tetra, 0, size - 1, 0);
-	cout << radiis.size() << endl;
-
-	/*
-	ofstream output_radi("radi.txt");
-	for (auto &i: radiis) {
-		output_radi << i << endl;
+	combination_tetra(perm_size, point_list, tetra, 0, size - 1, 0, output_radi);
+	if (!radiis.empty()) {
+		int remaining_size = dump_array(output_radi);
+		cout << "remaining " << remaining_size << " items written" << endl;
 	}
+
 	output_radi.close();
-	*/
 
 	// double r;
 	// Pt3D test = circumcenter_3d(point_list[5], point_list[7], point_list[8], point_list[9], r);
