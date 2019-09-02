f6e1e7825ba4fce79b97e321575ce435046fc168
LIU Hao <hliubs@rwcpu1.cse.ust.hk>
2019-09-02 20:32:03 +0800

    add index testing

--- core/out/exec.sh
+++ core/out/exec.sh
@@ -2,7 +2,26 @@
 
 # ./index.out /rwproject/kdd-db/hliubs/spatial/3dor/synthetic/pcd-001M.ply \
 # 250.0 800.0 802.0 /rwproject/kdd-db/hliubs/spatial/3dor/index/synthetic-pcd-001M.idx
+# ./index.out /rwproject/kdd-db/hliubs/spatial/3dor/synthetic/pcd-002M.ply \
+# 250.0 800.0 802.0 /rwproject/kdd-db/hliubs/spatial/3dor/index/synthetic-pcd-002M.idx
+# ./index.out /rwproject/kdd-db/hliubs/spatial/3dor/synthetic/pcd-005M.ply \
+# 250.0 800.0 802.0 /rwproject/kdd-db/hliubs/spatial/3dor/index/synthetic-pcd-005M.idx
+# ./index.out /rwproject/kdd-db/hliubs/spatial/3dor/synthetic/pcd-020M.ply \
+# 250.0 800.0 802.0 /rwproject/kdd-db/hliubs/spatial/3dor/index/synthetic-pcd-020M.idx
+# ./index.out /rwproject/kdd-db/hliubs/spatial/3dor/synthetic/pcd-100M.ply \
+# 250.0 800.0 802.0 /rwproject/kdd-db/hliubs/spatial/3dor/index/synthetic-pcd-100M.idx
 
-./query.out /rwproject/kdd-db/hliubs/spatial/3dor/synthetic/pcd-001M.ply \
-/rwproject/kdd-db/hliubs/spatial/3dor/index/synthetic-pcd-001M.idx \
-/rwproject/kdd-db/hliubs/spatial/3dor/synthetic/pcd-001M-q1.ply 0.05 0.9
+./index.out /rwproject/kdd-db/hliubs/spatial/3dor/synthetic/pcd-001M.ply \
+150.0 600.0 602.0 /rwproject/kdd-db/hliubs/spatial/3dor/index/synthetic-pcd-001M.idx
+# ./index.out /rwproject/kdd-db/hliubs/spatial/3dor/synthetic/pcd-002M.ply \
+# 250.0 800.0 802.0 /rwproject/kdd-db/hliubs/spatial/3dor/index/synthetic-pcd-002M.idx
+# ./index.out /rwproject/kdd-db/hliubs/spatial/3dor/synthetic/pcd-005M.ply \
+# 250.0 800.0 802.0 /rwproject/kdd-db/hliubs/spatial/3dor/index/synthetic-pcd-005M.idx
+# ./index.out /rwproject/kdd-db/hliubs/spatial/3dor/synthetic/pcd-020M.ply \
+# 250.0 800.0 802.0 /rwproject/kdd-db/hliubs/spatial/3dor/index/synthetic-pcd-020M.idx
+# ./index.out /rwproject/kdd-db/hliubs/spatial/3dor/synthetic/pcd-100M.ply \
+# 250.0 800.0 802.0 /rwproject/kdd-db/hliubs/spatial/3dor/index/synthetic-pcd-100M.idx
+
+# ./query.out /rwproject/kdd-db/hliubs/spatial/3dor/synthetic/pcd-001M.ply \
+# /rwproject/kdd-db/hliubs/spatial/3dor/index/synthetic-pcd-001M.idx \
+# /rwproject/kdd-db/hliubs/spatial/3dor/synthetic/pcd-001M-q1.ply 0.05 0.9
--- core/src/index.cpp
+++ core/src/index.cpp
@@ -3,11 +3,13 @@
 #include "tetra_meas.h"
 #include "pcr_adv.h"
 #include "RTree.h"
+#include "ProgressBar.hpp"
 #include <iostream>
 #include <fstream>
 #include <vector>
 #include <unordered_map>
 #include <chrono>
+#include <string>
 using namespace std;
 using namespace trimesh;
 
@@ -53,33 +55,31 @@ void add_ann_pts_in_cell(PtwID p, Cell c, double ann_min, double ann_max, vector
     }
 }
 
-vector<PtwID> get_ann_pts(PtwID p, unordered_map<int, Cell>* cells_map, double ann_min, double ann_max,
-    double w, int min_id[], int max_id[]) {
-
+vector<PtwID> get_ann_pts(PtwID p, double ann_min, double ann_max, const Grid* g) {
     vector<PtwID> ann_pts;
 
     int outer_id_min[3] = {
-        get_cell_id(p.pt.x - ann_max, w),
-        get_cell_id(p.pt.y - ann_max, w),
-        get_cell_id(p.pt.z - ann_max, w)
+        get_cell_id(p.pt.x - ann_max, g->w),
+        get_cell_id(p.pt.y - ann_max, g->w),
+        get_cell_id(p.pt.z - ann_max, g->w)
     };
     int outer_id_max[3] = {
-        get_cell_id(p.pt.x + ann_max, w),
-        get_cell_id(p.pt.y + ann_max, w),
-        get_cell_id(p.pt.z + ann_max, w)
+        get_cell_id(p.pt.x + ann_max, g->w),
+        get_cell_id(p.pt.y + ann_max, g->w),
+        get_cell_id(p.pt.z + ann_max, g->w)
     };
 
     for (int i = outer_id_min[0]; i <= outer_id_max[0]; i++) {
         for (int j = outer_id_min[1]; j <= outer_id_max[1]; j++) {
             for (int k = outer_id_min[2]; k <= outer_id_max[2]; k++) {
                 int cell_id[3] = { i, j, k };
-                int key = get_cell_key(cell_id, min_id, max_id);
+                int key = get_cell_key(cell_id, g->cell_id_min, g->cell_id_max);
 
-                auto it = cells_map->find(key);
-                if (it != cells_map->end()) {
+                auto it = g->cells_map.find(key);
+                if (it != g->cells_map.end()) {
                     // cell found
-                    if (max_dist(p.pt, it->second, w) - ann_min >= 0 &&
-                        min_dist(p.pt, it->second, w) - ann_max <= 0) {
+                    if (max_dist(p.pt, it->second, g->w) - ann_min >= 0 &&
+                        min_dist(p.pt, it->second, g->w) - ann_max <= 0) {
                         add_ann_pts_in_cell(p, it->second, ann_min, ann_max, ann_pts);
                     }
                 }
@@ -90,10 +90,9 @@ vector<PtwID> get_ann_pts(PtwID p, unordered_map<int, Cell>* cells_map, double a
     return ann_pts;
 }
 
-Entry cal_index_entry(PtwID p, unordered_map<int, Cell>* cells_map, double ann_min, double ann_max,
-    double w, int min_id[], int max_id[]) {
+Entry cal_index_entry(PtwID p, double ann_min, double ann_max, const Grid* g) {
 
-	vector<PtwID> ann_pts = get_ann_pts(p, cells_map, ann_min, ann_max, w, min_id, max_id);
+	vector<PtwID> ann_pts = get_ann_pts(p, ann_min, ann_max, g);
 
 	int hit_size = ann_pts.size();
     // cout << "Pt #" << p.id << " has " << hit_size << " ann pts" << endl;
@@ -131,9 +130,14 @@ Entry cal_index_entry(PtwID p, unordered_map<int, Cell>* cells_map, double ann_m
 
 int main(int argc, char **argv) {
 	if (argc < 6) {
-		cerr << "Usage: " << argv[0] << " database_filename w ann_min ann_max output_index_filename" << endl;
+		cerr << "Usage: " << argv[0] << " database_filename w ann_min ann_max output_index_filename [-test]" << endl;
 		exit(1);
 	}
+
+    bool test_mode = false;
+    if (argc == 7 && string(argv[6]) == "-test") {
+        test_mode = true;
+    }
 
 	string database_filename = argv[1];
 	TriMesh *mesh_p = TriMesh::read(database_filename);
@@ -145,106 +149,106 @@ int main(int argc, char **argv) {
     }
 
     double w = atof(argv[2]);
+    double delta = 0.3 * w;
+    int num_trials = test_mode ? 11 : 1;
+    Grid g;
 
-    mesh_p->need_bbox();
-    int cell_id_min[3] = {
-        get_cell_id(mesh_p->bbox.min[0], w),
-        get_cell_id(mesh_p->bbox.min[1], w),
-        get_cell_id(mesh_p->bbox.min[2], w)
-    };
-    int cell_id_max[3] = {
-        get_cell_id(mesh_p->bbox.max[0], w),
-        get_cell_id(mesh_p->bbox.max[1], w),
-        get_cell_id(mesh_p->bbox.max[2], w)
-    };
+    for (int counter = 0; counter < num_trials; counter++) {
+        double trial_w = w + counter * delta;
+        g.set_width(trial_w);
+        cout << "Gridify the point cloud..." << endl;
+        g.gridify(mesh_p);
 
-    unordered_map<int, Cell> cells_map;
-    for (int i = 0; i < n; i++) {
-        int cell_id[3] = {
-            get_cell_id(pts_p[i].x, w),
-            get_cell_id(pts_p[i].y, w),
-            get_cell_id(pts_p[i].z, w)    
-        };
-
-        int key = get_cell_key(cell_id, cell_id_min, cell_id_max);
-
-        auto it = cells_map.find(key);
-        if (it != cells_map.end()) {
-            // cell already exists
-            it->second.add_pt(i, pts_p[i]);
-        } else {
-            Cell new_cell(cell_id[0], cell_id[1], cell_id[2]);
-            new_cell.add_pt(i, pts_p[i]);
-            cells_map[key] = new_cell;
-        }
+        cout << "Grid size: " << trial_w << endl;
+        cout << "Total # cells: " << g.cells_count << endl;
+        cout << "Avg # pts per cell: " << ((double) n) / ((double) g.cells_count) << endl;
     }
 
-    int cells_count = cells_map.size();
-    cout << "Total # of cells: " << cells_count << endl;
-
-    auto t1 = chrono::high_resolution_clock::now();
+    // auto t1 = chrono::high_resolution_clock::now();
 
     double ann_min = atof(argv[3]);
     double ann_max = atof(argv[4]);
+    vector<Entry> entry_list;
 
-    string outidx_filename = argv[5];
-    string outgrid_filename = outidx_filename + ".grid";
-    ofstream outgrid_ifs(outgrid_filename);
+    int failed_entry_count = 0;
+    cout << "Calculating index entry for every point..." << endl;
+    ProgressBar bar(n, 70);
+    for (int i = 0; i < n; i++) {
+        ++bar;
 
-    // write grid headers
-    outgrid_ifs << w << " " << ann_min << " " << ann_max << " " << cells_count << endl;
-    outgrid_ifs << cell_id_min[0] << " " << cell_id_min[1] << " " << cell_id_min[2] << " "
-                << cell_id_max[0] << " " << cell_id_max[1] << " " << cell_id_max[2] << endl;
-
-    RTree<int, double, 2> tree;
-
-    int fail_count = 0;
-    int counter = 0;
-    int check_point = cells_count / 100;
-    int exec_point = 57376;
-    for (auto it = cells_map.begin(); it != cells_map.end(); it++) {
-        counter++;
-        // if (counter != exec_point) {
-        //     continue;
+        PtwID p(i, pts_p[i]);
+        Entry e = cal_index_entry(p, ann_min, ann_max, &g);
+        entry_list.push_back(e);
+        if (e.fail) {
+            failed_entry_count++;
+        }
+        // if (i >= 100) {
+        //     break;
         // }
-        int key = it->first;
-    	Cell c = it->second;
-        // cout << "Processing cell: " << c.x << ", " << c.y << ", " << c.z << endl;
-        
-        // output per-cell information
-        outgrid_ifs << key << " " << c.to_str() << endl;;
 
-    	Entry prem_entry;
-        prem_entry.fail = true;
-        for (PtwID &p: c.list) {
-    		Entry e = cal_index_entry(p, &cells_map, ann_min, ann_max, w, cell_id_min, cell_id_max);
-    		// find premium entry having the largest meas
-            if (e.meas > prem_entry.meas && e.fail == false) {
-    			prem_entry = e;
-    		}
-    	}
-
-        // output per-cell premium entry
-        outgrid_ifs << prem_entry.to_str() << endl;
-
-        if (prem_entry.fail) {
-            fail_count++;
-        } else {
-            double box_min[2] = { prem_entry.vol, prem_entry.meas },
-                   box_max[2] = { prem_entry.vol, prem_entry.meas };
-            tree.Insert(box_min, box_max, key);
-        }
-
-        if (counter % check_point == 0) {
-            cout << (counter / check_point) << "/100" << endl;
-        }
+        bar.display();
     }
-    cout << "Total # of failed cells: " << fail_count << endl;
+    bar.done();
+    cout << failed_entry_count << " out of " << n << " failed" << endl;
 
-    outgrid_ifs.close();
-    tree.Save(outidx_filename.c_str());
+    // string outidx_filename = argv[5];
+    // string outgrid_filename = outidx_filename + ".grid";
+    // ofstream outgrid_ifs(outgrid_filename);
 
-    auto t2 = chrono::high_resolution_clock::now();
-    chrono::duration<double, milli> span = t2 - t1;
-    cout << "Total time: " << span.count() << endl;
+    // // write grid headers
+    // outgrid_ifs << w << " " << ann_min << " " << ann_max << " " << cells_count << endl;
+    // outgrid_ifs << cell_id_min[0] << " " << cell_id_min[1] << " " << cell_id_min[2] << " "
+    //             << cell_id_max[0] << " " << cell_id_max[1] << " " << cell_id_max[2] << endl;
+
+    // RTree<int, double, 2> tree;
+
+    // int fail_count = 0;
+    // int counter = 0;
+    // int check_point = cells_count / 100;
+    // int exec_point = 57376;
+    // for (auto it = cells_map.begin(); it != cells_map.end(); it++) {
+    //     counter++;
+    //     // if (counter != exec_point) {
+    //     //     continue;
+    //     // }
+    //     int key = it->first;
+    // 	Cell c = it->second;
+    //     // cout << "Processing cell: " << c.x << ", " << c.y << ", " << c.z << endl;
+        
+    //     // output per-cell information
+    //     outgrid_ifs << key << " " << c.to_str() << endl;;
+
+    // 	Entry prem_entry;
+    //     prem_entry.fail = true;
+    //     for (PtwID &p: c.list) {
+    // 		Entry e = cal_index_entry(p, &cells_map, ann_min, ann_max, w, cell_id_min, cell_id_max);
+    // 		// find premium entry having the largest meas
+    //         if (e.meas > prem_entry.meas && e.fail == false) {
+    // 			prem_entry = e;
+    // 		}
+    // 	}
+
+    //     // output per-cell premium entry
+    //     outgrid_ifs << prem_entry.to_str() << endl;
+
+    //     if (prem_entry.fail) {
+    //         fail_count++;
+    //     } else {
+    //         double box_min[2] = { prem_entry.vol, prem_entry.meas },
+    //                box_max[2] = { prem_entry.vol, prem_entry.meas };
+    //         tree.Insert(box_min, box_max, key);
+    //     }
+
+    //     if (counter % check_point == 0) {
+    //         cout << (counter / check_point) << "/100" << endl;
+    //     }
+    // }
+    // cout << "Total # of failed cells: " << fail_count << endl;
+
+    // outgrid_ifs.close();
+    // tree.Save(outidx_filename.c_str());
+
+    // auto t2 = chrono::high_resolution_clock::now();
+    // chrono::duration<double, milli> span = t2 - t1;
+    // cout << "Total time: " << span.count() << endl;
 }
